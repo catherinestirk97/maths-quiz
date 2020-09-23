4c08574454ceb1a3ee88f0b59e93edbdcd36013f
catherinestirk97 <53531093+catherinestirk97@users.noreply.github.com>
2020-09-23 16:37:20 +0100

    Initial Commit

---
+++ addition.js
@@ -0,0 +1,34 @@
+//using gitHubDesktop 
+
+let x = (Math.round(Math.random() * 12));
+let y = (Math.round(Math.random() * 12));  
+let total = x+y;
+
+document.getElementById("calculation").innerHTML = `${x} + ${y}`
+
+function generateNewNumber(){
+    x = (Math.round(Math.random() * 12));
+    y = (Math.round(Math.random() * 12)); 
+    total = x+y;
+    document.getElementById("calculation").innerHTML = `${x} + ${y}`
+}
+
+function reset(){
+    document.getElementById("userInput").value = "";
+}
+
+function checkAnswer(){
+    let answer = parseInt(document.getElementById("userInput").value);
+    console.log(answer);
+if(total === answer){
+    alert("Congratulations! That was the right number");
+    generateNewNumber();
+    reset();
+} else {
+    alert("Sorry that was incorrect! Please try again");
+    console.log(answer)
+    reset();
+}
+}
+
+let hello = alert("hello github Desktop");
