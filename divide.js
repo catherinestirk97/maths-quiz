3bd1576fb5cc38ca22f72c81f6330e40b9a8fb66
catherinestirk97 <53531093+catherinestirk97@users.noreply.github.com>
2020-09-24 10:13:33 +0100

    Add divide page

---
+++ divide.js
@@ -0,0 +1,36 @@
+let x = (Math.round(Math.random() * 11)+1);
+// console.log(x);
+let y = (Math.round(Math.random() * 11)+1);  
+// console.log(y);
+let z = x*y;
+// console.log(z);
+let total = z/x;
+
+
+document.getElementById("calculation").innerHTML = `${z} / ${x}`
+
+function generateNewNumber(){
+    x = (Math.round(Math.random() * 11)+1);
+    y = (Math.round(Math.random() * 11)+1);
+    z =x*y;
+    total = z/x;
+    document.getElementById("calculation").innerHTML = `${z} / ${x}`
+}
+
+function reset(){
+    document.getElementById("userInput").value = "";
+}
+
+function checkAnswer(){
+    let answer = parseInt(document.getElementById("userInput").value);
+    console.log(answer);
+if(total === answer){
+    alert("Congratulations! That was the right answer.");
+    generateNewNumber();
+    reset();
+} else {
+    alert("Sorry that was incorrect! Please try again.");
+    console.log(answer)
+    reset();
+}
+}
